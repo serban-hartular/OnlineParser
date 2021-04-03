ad1f4478a755780f2a7a489ce80b4da7dab2a079
serban-hartular <hartular@gmail.com>
2021-04-03 18:59:11 +0300

    Added flask hello world

---
+++ flask_app.py
@@ -0,0 +1,18 @@
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+   return 'Hello World'
+
+@app.route('/hello')
+def hello_subdir():
+   return 'hello subdirectory!'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+
+if __name__ == '__main__':
+   app.run()
